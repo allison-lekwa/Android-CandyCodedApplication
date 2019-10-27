65774af7255c7ca6921cc52d18e1f6ae77fd73bf
Allison Onyeije <50016516+allison-lekwa@users.noreply.github.com>
2019-10-27 16:19:16 +0100

    Add implicit and explicit intents to th android CandyStore app

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,11 +1,15 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import static android.net.Uri.parse;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        Uri uri = parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
@@ -30,4 +34,21 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    // The createMapIntent method -->
+
+    public void createMapIntent(View view){
+
+        //creates the Uri
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        // creates the mapIntent
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+
+        //make the intent explicit
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        mapIntent.resolveActivity(getPackageManager());
+        startActivity(mapIntent);
+    }
 }
